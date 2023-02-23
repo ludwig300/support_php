803de0636231f31149cf51e57e0310a0dc8c5be0
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-02-23 19:13:26 +1000

    Update Admin panel.

--- supportphp/orderbase/admin.py
+++ supportphp/orderbase/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Maker, Order
+
+
+@admin.register(Client)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'subscription_is_active')
+
+
+@admin.register(Maker)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'subscription_is_active')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    search_fields = ('order_datetime', 'name', 'client', 'maker', 'order_is_done')
