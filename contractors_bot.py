5df6cc93cec2b89eb84efef72ce61190780b2fd7
Ilya <Dragoilya@yandex.ru>
2023-02-23 10:07:07 +0300

    Initialized the intro functions for the contractors_bot

---
+++ contractors_bot.py
@@ -0,0 +1,74 @@
+import logging
+import time
+
+import telebot
+from environs import Env
+
+# Настройки логирования
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+env = Env()
+env.read_env(override=True)
+bot = telebot.TeleBot(env.str("TELEGRAM_CONTRSCTORS_BOT_API_TOKEN"))
+
+# Словарь для хранения сообщений от пользователей
+user_messages = {}
+
+def get_access(user_id):
+    if 0: # пользователя нет в БД
+        return 0
+    elif 0: # пользователь есть в БД, но не оплатил подписку
+        return 1
+    else: # пользователь есть в БД и оплатил подписку
+        return 2
+
+
+@telebot.message_handler(commands=['start'])
+def bot_start(message):
+    access, contractor = get_access(message.chat.id)
+    if access:
+        if access==1: # пользователь есть в БД, но не оплатил подписку
+            bot.send_message(
+                message.chat.id,
+                f"Приветствую, {contractor.name}",
+                reply_markup=get_bot_menu_keyboard_for_1()
+            )
+        else: # пользователь есть в БД и оплатил подписку
+            bot.send_message(
+                message.chat.id,
+                f"Приветствую, {contractor.name}",
+                reply_markup=get_bot_menu_keyboard_for_2()
+            )
+    else: # пользователя нет в БД
+        bot.send_message(
+            message.chat.id,
+            f"Приветствую",
+            reply_markup=get_bot_menu_keyboard_for_0()
+        )
+
+
+def get_bot_menu_keyboard_for_0():
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(telebot.types.KeyboardButton('Ввести Имя'))
+    return keyboard
+
+
+def get_bot_menu_keyboard_for_1():
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(telebot.types.KeyboardButton('Оплатить подписку'))
+    return keyboard
+
+
+def get_bot_menu_keyboard_for_2():
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(telebot.types.KeyboardButton('Вывести список заказов'))
+    return keyboard
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
